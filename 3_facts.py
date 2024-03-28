dc9d507f78af36d6fe6514f3eb56729a3087257a
Adrian Ponce <adrian.ponce.89@gmail.com>
2024-03-28 19:47:11 -0300

    Split document in chunks

--- 3_facts.py
+++ 3_facts.py
@@ -1,14 +1,26 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 # Load the environment variables
 load_dotenv()
 
+# Split the text into characters
+text_splitter = CharacterTextSplitter(
+  separator="\n",
+  chunk_size=200,
+  chunk_overlap=0
+)
+
 # Load the text file
 loader = TextLoader("assets/facts.txt")
 
 # Load the documents
-docs = loader.load()
+docs = loader.load_and_split(
+  text_splitter=text_splitter
+)
 
 # Print the documents
-print(docs)
+for doc in docs:
+  print(doc.page_content)
+  print("\n")
