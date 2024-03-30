0bcb8049e488772d2e31ebba5c06bac9f1883ca2
Adrian Ponce <adrian.ponce.89@gmail.com>
2024-03-30 11:08:25 -0300

    Facts: add embbeding with similarity search

--- 3_facts.py
+++ 3_facts.py
@@ -1,9 +1,13 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 # Load the environment variables
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
 
 # Split the text into characters
 text_splitter = CharacterTextSplitter(
@@ -20,7 +24,15 @@ docs = loader.load_and_split(
   text_splitter=text_splitter
 )
 
-# Print the documents
-for doc in docs:
-  print(doc.page_content)
+db = Chroma.from_documents(
+  docs,
+  embedding=embeddings,
+  persist_directory="emb"
+)
+
+results = db.similarity_search_with_score("What is a interesting fact about the English language?")
+
+for result in results:
   print("\n")
+  print(result[1])
+  print(result[0].page_content)
