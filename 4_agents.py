2aec65e65051b88de792836e39f94c52e8ee1adf
Adrian Ponce <adrian.ponce.89@gmail.com>
2024-04-01 20:53:18 -0300

    Add ConversationBufferMemory to AgentExecutor

--- 4_agents.py
+++ 4_agents.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.slq import run_query_tool, list_tables, describe_tables_tool
@@ -26,9 +27,15 @@ chat_prompt = ChatPromptTemplate(
       "Do not make any assumptions about the schema of the tables. "
       "Intead, use the 'describe_tables' tool to get the schema of a table.\n"
       )),
+    MessagesPlaceholder(variable_name="chat_history"),
     HumanMessagePromptTemplate.from_template("{input}"),
     MessagesPlaceholder(variable_name="agent_scratchpad")
   ]
+)
+
+memory = ConversationBufferMemory(
+  memory_key="chat_history",
+  return_messages=True # Return the messages to the agent as objects instead of strings
 )
 
 tools = [
@@ -46,8 +53,11 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
   agent=agent,
   verbose=True,
-  tools=tools
+  tools=tools,
+  memory=memory
 )
 
-#agent_executor("How many users have provided their shiping address?")
-agent_executor("Summarize the top 5 most popular products with their names. Write the report to 'top_products.html'")
+# agent_executor("How many users have provided their shiping address?")
+# agent_executor("Summarize the top 5 most popular products with their names. Write the report to 'top_products.html'")
+agent_executor("How many orders there are?")
+agent_executor("Repeat the same procces for users")
