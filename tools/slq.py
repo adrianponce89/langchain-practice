508f48eb8d4743b04bda73d2b1723508caf6c472
Adrian Ponce <adrian.ponce.89@gmail.com>
2024-04-01 20:16:56 -0300

    Add arg schema

--- tools/slq.py
+++ tools/slq.py
@@ -1,4 +1,6 @@
 import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
 from langchain.tools import Tool
 
 conn = sqlite3.connect('db.sqlite')
@@ -17,10 +19,14 @@ def run_sqlite_query(query):
     except sqlite3.OperationalError as err:
         return f"The following error occured: {str(err)}"
 
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
 run_query_tool = Tool.from_function(
     name='run_sqlite_query',
     description='Run a query on the sqlite database',
     func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema,
 )
 
 def describe_tables(table_names):
@@ -29,8 +35,12 @@ def describe_tables(table_names):
     rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});").fetchall()
     return '\n'.join(row[0] for row in rows if row[0] is not None)
 
+class DescribeTablesArgsSchema(BaseModel):
+    table_names: List[str]
+
 describe_tables_tool = Tool.from_function(
     name='describe_tables',
     description='Given a list of table names, returns the schema of the tables',
     func=describe_tables,
+    args_schema=DescribeTablesArgsSchema,
 )
